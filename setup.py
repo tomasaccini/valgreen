46c0af487a3e9995bd5f47f99331e704d938e6a5
FdelMazo <fdelmazo@fi.uba.ar>
2020-01-16 11:10:21 -0300

    Releases version 0.4 -- accept command line args

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 
 setup(name='valgreen',
-    version='0.3',
+    version='0.4',
     description='Valgrind output readable for human beings ',
     long_description=readme(),
     url='https://github.com/tomasaccini/valgreen/',
